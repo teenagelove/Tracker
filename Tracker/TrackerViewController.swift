1e334401fefa8aead3cb626fc00434e21e6cfda4
teenagelove <dani4upa586@gmail.com>
2025-03-25 21:37:42 +0900

    feat(tracker): add date picker and setup navigation bar

--- Tracker/TrackerViewController.swift
+++ Tracker/TrackerViewController.swift
@@ -9,18 +9,10 @@ import UIKit
 
 final class TrackerViewController: UIViewController {
     // MARK: - UI Components
-    private lazy var addButton: UIButton = {
-        let button = UIButton()
-        button.setImage(.plusButton, for: .normal)
-        button.imageView?.contentMode = .scaleAspectFit
-        return button
-    }()
-    
-    private lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = Constants.UIString.trackers
-        label.font = Constants.Fonts.title
-        return label
+    private lazy var datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        return datePicker
     }()
     
     private lazy var emptyStateImageView: UIImageView = {
@@ -51,27 +43,29 @@ final class TrackerViewController: UIViewController {
 private extension TrackerViewController {
     func setupUI() {
         view.backgroundColor = .ypBackground
-        navigationController?.setNavigationBarHidden(true, animated: true)
-        view.addSubviews(addButton, emptyStateLabel, emptyStateImageView, titleLabel)
+        view.addSubviews(emptyStateLabel, emptyStateImageView)
+        setupNavigationBar()
         setupConstraints()
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.title = Constants.UIString.trackers
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: .plusButton, style: .plain, target: self, action: nil)
+        navigationItem.leftBarButtonItem?.tintColor = .ypBlack
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: datePicker)
     }
 
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            addButton.heightAnchor.constraint(equalToConstant: 44),
-            addButton.widthAnchor.constraint(equalToConstant: 44),
-            addButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 1),
-            addButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 6),
-            
             emptyStateImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             emptyStateImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
             emptyStateLabel.topAnchor.constraint(equalTo: emptyStateImageView.bottomAnchor, constant: 8),
             emptyStateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             emptyStateLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            
-            titleLabel.topAnchor.constraint(equalTo: addButton.bottomAnchor, constant: 1),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
         ])
     }
 }
